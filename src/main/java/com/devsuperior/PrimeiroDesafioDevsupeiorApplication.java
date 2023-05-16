fa2f04c30c6878bd485772b70ee87419be8d40b7
Ivanfjr1 <ivanfjr1@gmail.com>
2023-05-16 20:21:07 -0300

    Adicionado Scanner

--- src/main/java/com/devsuperior/PrimeiroDesafioDevsupeiorApplication.java
+++ src/main/java/com/devsuperior/PrimeiroDesafioDevsupeiorApplication.java
@@ -1,5 +1,8 @@
 package com.devsuperior;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,16 +23,35 @@ public class PrimeiroDesafioDevsupeiorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("##########################");
+		System.out.println("########## Exemplo ################");
 		orderExample(new Order(1034, 150.00, 20.00));
 		orderExample(new Order(2282, 800.00, 10.00));
 		orderExample(new Order(1309, 95.90, 0.00));
+		System.out.println("######### Fim Exemplo #################");
+		
+		orderScanner();
 	}
 	
 	private void orderExample(Order order) {
 		System.out.println(String.format("Pedido código %d", order.getCode()));
 		System.out.println(String.format("Valor total: R$ %s", orderService.total(order)));
 		System.out.println("##########################");
+	}
+	
+	private void orderScanner() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Código: ");
+		Integer code = sc.nextInt();
+		System.out.print("Valor base: ");
+		double basic = sc.nextDouble();
+		System.out.printf("Desconto: ");
+		double discount = sc.nextDouble();
+		
+		System.out.println("##########################");
+		orderExample(new Order(code, basic, discount));
+		
+		sc.close();
 	}
 
 }
